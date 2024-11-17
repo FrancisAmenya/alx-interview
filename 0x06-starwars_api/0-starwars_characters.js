96e2532a996b567a94eec1f41732528a8bc9cacc
FrancisAmenya <amenyafrancis@gmail.com>
2024-11-17 22:54:06 +0000

    fixed request

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,53 +1,17 @@
 #!/usr/bin/node
+
 const request = require('request');
-const movieId = process.argv[2];
 
-if (!movieId) {
-  console.log('Enter movie ID');
-  process.exit(1);
-}
-
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-const fetchMovie = (url) => {
-  return new Promise((resolve, reject) => {
-    request.get(url, (err, res, body) => {
-      if (err) {
-        reject(err);
-      } else {
-        const movie = JSON.parse(body);
-        resolve(movie);
-      }
-    });
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
 };
-
-const fetchCharacters = (movie) => {
-  return Promise.all(
-    movie.characters.map(async (characterUrl) => {
-      return new Promise((resolve, reject) => {
-        request.get(characterUrl, (err, res, body) => {
-          if (err) {
-            reject(err);
-          } else {
-            const character = JSON.parse(body);
-            resolve(character.name);
-          }
-        });
-      });
-    })
-  );
-};
-
-const main = async () => {
-  try {
-    const movie = await fetchMovie(apiUrl);
-    const characters = await fetchCharacters(movie);
-
-    characters.forEach((actor) => console.log(actor));
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-main();
